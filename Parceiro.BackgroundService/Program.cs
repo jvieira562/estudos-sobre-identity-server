bb58743d72d5491ab0996f955090a91e7a3f77a6
jvieira <contato.josevictorvieira@gmail.com>
2023-09-29 07:25:53 -0300

    Finalizando curso

---
+++ Parceiro.BackgroundService/Program.cs
@@ -0,0 +1,61 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Parceiro.BackgroundService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "657460f846e64493b923b88632e3e41e",
+                ClientSecret = "69ec6a8e4c6f44b0bb1e254e1a326560",
+                Scope = "api_frete.read_only"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:5007/fretes/para/-23.3489854,-51.1484878/calcular?Altura=9.6&Largura=0.83&Comprimento=31.09&Peso=3");
+            if (!response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content + "\n\n");
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
